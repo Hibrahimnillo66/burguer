a8ce2b70d18ec03ca4f35ac7fffb3a0b473684ed
AbrahamG <abraham.olague@gmail.com>
2019-06-22 09:53:17 -0500

    database added

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,20 @@
 var mysql = require("mysql");
-
+var connection;
 //Define database connection properties (host, user, password, and database name)
 //Use production database when deployed.
 if (process.env.JAWSDB_URL) {
   //Heroku deployment
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
-
-var connection = mysql.createConnection({
+else {
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "root",
     database: "burgers_db"
   });
-  
+};
   // Make connection.
   connection.connect(function(err) {
     if (err) {
